4e12d0ec4c3e09f65ffca89d5a2a51d1a0b2b7f0
nazarpelekh <nazar_peleh@meta.ua>
2017-08-02 12:42:48 +0300

    Modify custom2component & "Firebase"

--- src/app/custom2/custom2.component.ts
+++ src/app/custom2/custom2.component.ts
@@ -9,9 +9,21 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 
 export class Custom2Component {
 
-  items: FirebaseListObservable<any[]>;
+  items: FirebaseListObservable<any>;
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/items');
+  }
+  addItem(newName: string) {
+    this.items.push({ text: newName });
+  }
+  updateItem(key: string, newText: string) {
+    this.items.update(key, { text: newText });
+  }
+  deleteItem(key: string) {
+    this.items.remove(key);
+  }
+  deleteEverything() {
+    this.items.remove();
   }
 
 }
