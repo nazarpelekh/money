0a9730a698a28e5f931a3fb8d7997abdb3412380
nazarpelekh <nazar_peleh@meta.ua>
2017-08-04 16:30:16 +0300

    Add  simple checklist

--- src/app/custom3/custom3.component.ts
+++ src/app/custom3/custom3.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export class Item {
+  purchase: string;
+  done: boolean;
+  price: number;
+
+  constructor(purchase: string, price: number) {
+
+    this.purchase = purchase;
+    this.price = price;
+    this.done = false;
+  }
+}
 
 @Component({
   selector: 'app-custom3',
   templateUrl: './custom3.component.html',
   styleUrls: ['./custom3.component.scss']
 })
-export class Custom3Component implements OnInit {
 
-  constructor() { }
+export class Custom3Component {
+  items: Item[] =
+      [
+        { purchase: "Хлеб", done: false, price: 15.9 },
+        { purchase: "Масло", done: false, price: 60 },
+        { purchase: "Картофель", done: true, price: 22.6 },
+        { purchase: "Сыр", done: false, price:310 }
+      ];
+  addItem(text: string, price: number): void {
 
-  ngOnInit() {
+    if(text==null || text==undefined || text.trim()=="")
+      return;
+    if(price==null || price==undefined)
+      return;
+    this.items.push(new Item(text, price));
+  }
+
+  deleteItem(item: Item): void {
+    if (item !== undefined && item !== null) {
+      let index: number = this.items.indexOf(item, 0);
+
+      if (index > -1) {
+        this.items.splice(index, 1);
+      }
+    }
   }
 
 }
