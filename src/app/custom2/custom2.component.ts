af5de5b43a909f836ace7d32d65597bd7e132769
nazarpelekh <nazar_peleh@meta.ua>
2017-08-09 11:52:07 +0300

    Change Component2

--- src/app/custom2/custom2.component.ts
+++ src/app/custom2/custom2.component.ts
@@ -10,14 +10,17 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 export class Custom2Component {
 
   items: FirebaseListObservable<any>;
+  cash: FirebaseListObservable<any>;
   constructor(db: AngularFireDatabase) {
     this.items = db.list('/items');
+    this.cash = db.list('/cash');
   }
   addItem(newName: string) {
     this.items.push({ text: newName });
   }
   updateItem(key: string, newText: string) {
     this.items.update(key, { text: newText });
+    this.cash.update(key, { text: newText });
   }
   deleteItem(key: string) {
     this.items.remove(key);
